a8209f96aedd8552ece65f55586ffda63a9763e5
debtanu-capitalnumbers <debtanu@capitalnumbers.com>
2024-02-03 01:06:33 +0530

    changes on blur

--- src/components/events/inputs/EventStartDate.vue
+++ src/components/events/inputs/EventStartDate.vue
@@ -1,7 +1,7 @@
 <template>   
     <div class="row p-3 col-md-10 form-group required">
         <span for="event_start_date" class="control-label">Start Date</span>
-        <VueDatePicker ref="event_start_date" v-model="props.form.event_start_date" @keyup="validateData('event_start_date')" :class="{ 'is-invalid': props.errors.event_start_date && props.errors.event_start_date[0] }" :enable-time-picker="false" :clearable="false" class="form-control border-0 p-0" id="event_start_date" name="event_start_date"></VueDatePicker>
+        <VueDatePicker ref="event_start_date" v-model="props.form.event_start_date" @blur="validateData('event_start_date')" :class="{ 'is-invalid': props.errors.event_start_date && props.errors.event_start_date[0] }" :enable-time-picker="false" :clearable="false" class="form-control border-0 p-0" id="event_start_date" name="event_start_date"></VueDatePicker>
         <div class="invalid-feedback" v-if="props.errors.event_start_date && props.errors.event_start_date[0]">
             {{ props.errors.event_start_date && props.errors.event_start_date[0] }}
         </div>
--- src/components/events/inputs/EventStartTime.vue
+++ src/components/events/inputs/EventStartTime.vue
@@ -1,7 +1,7 @@
 <template>   
     <div class="row p-3 col-md-10 form-group required">
         <span for="event_start_time" class="control-label">Start Time</span>
-        <VueDatePicker ref="event_start_time" v-model="props.form.event_start_time" @keyup="validateData('event_start_time')" :class="{ 'is-invalid': props.errors.event_start_time && props.errors.event_start_time[0] }" time-picker :clearable="false" class="form-control border-0 p-0" id="event_start_time" name="event_start_time"></VueDatePicker>
+        <VueDatePicker ref="event_start_time" v-model="props.form.event_start_time" @blur="validateData('event_start_time')" :class="{ 'is-invalid': props.errors.event_start_time && props.errors.event_start_time[0] }" time-picker :clearable="false" class="form-control border-0 p-0" id="event_start_time" name="event_start_time"></VueDatePicker>
         <div class="invalid-feedback" v-if="props.errors.event_start_time && props.errors.event_start_time[0]">
             {{ props.errors.event_start_time && props.errors.event_start_time[0] }}
         </div>
